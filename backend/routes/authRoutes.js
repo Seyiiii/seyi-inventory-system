d1f7fee06b2f91eefde0abd9960b61fc47ec8579
Seyiii <ainaseyim@gmail.com>
2026-06-02 21:36:13 +0100

    Fixed Delete Users Bug

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { registerUser, loginUser, getAllUsers, updateUserRole } from '../controllers/authController.js';
+import { registerUser, loginUser, getAllUsers, updateUserRole, deleteUser } from '../controllers/authController.js';
 import { protect, authorize } from '../middlewares/authMiddleware.js';
 
 
@@ -9,5 +9,6 @@ router.post('/register', registerUser);
 router.post('/login', loginUser);
 router.get('/users', protect, authorize('super_admin', 'admin'), getAllUsers);
 router.put('/users/:id/role', protect, authorize('super_admin', 'admin'), updateUserRole);
+router.delete('/users/:id', protect, authorize('super_admin', 'admin'), deleteUser);
 
 export default router;
